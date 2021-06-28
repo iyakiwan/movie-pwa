4cdc8a0569663aaee3844ab52de1b721c0613788
Mufti Alie S <iyakiwan19@gmail.com>
2021-06-29 00:33:22 +0700

    Update IndexDb

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import UrlParser from '../../routes/url-parser';
 import TheMovieDbSource from '../../data/themoviedb-source';
 import { createMovieDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
     return `
         <div id="movie" class="movie"></div>
+        <div id="likeButtonContainer"></div>
       `;
   },
 
@@ -15,6 +17,16 @@ const Detail = {
     const movieContainer = document.querySelector('#movie');
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
     // Fungsi ini akan dipanggil setelah render()
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
